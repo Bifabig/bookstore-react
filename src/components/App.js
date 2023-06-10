7b45292f753200f20175ad403dbf7fc33e9472a8
Bifabig <biftubig@gmail.com>
2023-06-10 11:24:33 +0300

    add categories path

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router-dom';
 import Books from './Books';
 import Navbar from './Navbar';
 import '../styles/App.module.scss';
+import Categories from './Categories';
 
 const BookstoreApp = () => (
   <Routes>
     <Route path="/" element={<Navbar />}>
       <Route index element={<Books />} />
+      <Route path="categories" element={<Categories />} />
     </Route>
   </Routes>
 );
